592f8b5276825c8fac56007540466e8a50322e93
Sergey <super.super.spore.com97@gmail.com>
2023-07-30 15:43:27 +0900

    append new project folder 'Lesson_3'

---
+++ Lesson_3/src/main/java/org/example/DoublyLinkedList.java
@@ -0,0 +1,53 @@
+package org.example;
+
+class DoublyLinkedList {
+    Node head;
+    Node tail;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    // Метод для добавления нового узла в конец списка
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.previous = tail;
+            tail = newNode;
+        }
+    }
+
+    // Метод для разворота двусвязного списка
+    public void revert() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            Node next = currentNode.next;
+            Node previous = currentNode.previous;
+            currentNode.next = previous;
+            currentNode.previous = next;
+
+            if (previous == null) {
+                tail = currentNode;
+            }
+            if (next == null) {
+                head = currentNode;
+            }
+            currentNode = next;
+        }
+    }
+
+    // Метод для отображения списка
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
---
+++ Lesson_3/src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node previous;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.previous = null;
+        this.next = null;
+    }
+}
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random ran = new Random();
+        System.out.print("Введите длину списка: ");
+        int length = scanner.nextInt();
+        DoublyLinkedList list = new DoublyLinkedList();
+
+        for(int i = 0; i < length; ++i) {
+            list.addNode(ran.nextInt(1, length * 2));
+        }
+
+        System.out.println("Исходный список:");
+        list.display();
+        list.revert();
+        System.out.println("Развёрнутый список:");
+        list.display();
+    }
+}
